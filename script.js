6827b22a4b996e3303ea14a0af47e2ca88ef3b6a
Renan-Rezende <112997019+Renan-Rezende@users.noreply.github.com>
2023-05-11 13:00:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function InserirAluno() {
-  var Matricula = prompt("Insira a matricula do aluno");
-  var Nome = prompt("Insira o nome do aluno");
-  var Faltas = parseInt(prompt("Insira as faltas do aluno"));
-  var P1 = parseFloat(prompt("Insira  a nota P1 do aluno"));
-  var P2 = parseFloat(prompt("Insira a nota P2 do aluno"));
+  var Matricula = parseFloat(document.getElementById("matricula").value);
+  var Nome = document.getElementById("nome").value;
+  var Faltas = parseFloat(document.getElementById("faltas").value);
+  var P1 = parseFloat(document.getElementById("P1").value);
+  var P2 = parseFloat(document.getElementById("P2").value);
   var Média = (P1 + P2) / 2;
   var PR = 0;
   var MédiaFinal = 0;
@@ -18,7 +18,7 @@ function InserirAluno() {
       MédiaFinal = Média;
       Situação = "Aprovado";
     } else {
-      PR = parseFloat(prompt("Digite a nota da PR do aluno"));
+      PR = parseFloat(document.getElementById("PR").value);
       MédiaFinal = (Média + PR) / 2;
       if (MédiaFinal >= 7) {
         Situação = "Aprovado";
@@ -39,4 +39,11 @@ function InserirAluno() {
   linha.insertCell(6).innerText = PR;
   linha.insertCell(7).innerText = MédiaFinal;
   linha.insertCell(8).innerText = Situação;
+
+  document.getElementById("matricula").value = "";
+  document.getElementById("nome").value = "";
+  document.getElementById("faltas").value = "";
+  document.getElementById("P1").value = "";
+  document.getElementById("P2").value = "";
+  document.getElementById("PR").value = "";
 }
